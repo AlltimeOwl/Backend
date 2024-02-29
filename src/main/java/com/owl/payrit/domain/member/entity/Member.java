bce5d91cd5a5e964324c448d47358d9d6d12739d
Shin-sangwon <dnjsml10@gmail.com>
2024-02-29 11:25:39 +0900

    feat: pull base settings

--- src/main/java/com/owl/payrit/domain/member/entity/Member.java
+++ src/main/java/com/owl/payrit/domain/member/entity/Member.java
@@ -1,23 +1,22 @@
 package com.owl.payrit.domain.member.entity;
 
+import com.owl.payrit.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Entity
-public class Member {
-
-    @Id
-    private Long id;
+public class Member extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private OauthProvider oauthProvider;
