8b2b441ed8283bf0fdd5b0f2cb877a855ee46796
Shin-sangwon <dnjsml10@gmail.com>
2024-02-29 22:01:53 +0900

    feat: kakao api 등통신을 위한 interface 구현

---
+++ src/main/java/com/owl/payrit/domain/auth/configuration/HttpInterfaceConfiguration.java
@@ -0,0 +1,23 @@
+package com.owl.payrit.domain.auth.configuration;
+
+import com.owl.payrit.domain.auth.api.KakaoApiClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class HttpInterfaceConfiguration {
+
+    @Bean
+    public KakaoApiClient kakaoApiClient() {
+        return createHttpInterface(KakaoApiClient.class);
+    }
+
+    private <T> T createHttpInterface(Class<T> clazz) {
+        WebClient webClient = WebClient.create();
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
+        return httpServiceProxyFactory.createClient(clazz);
+    }
+}
