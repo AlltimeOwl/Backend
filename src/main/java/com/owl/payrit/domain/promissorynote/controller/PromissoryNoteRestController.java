5409a4103683c32895693bbdcc70792ae6685ffb
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2024-03-02 16:44:52 +0900

    feat: promissoryNote 생성 컨트롤러 매핑 구상

--- src/main/java/com/owl/payrit/domain/promissorynote/controller/PromissoryNoteRestController.java
+++ src/main/java/com/owl/payrit/domain/promissorynote/controller/PromissoryNoteRestController.java
@@ -1,13 +1,29 @@
 package com.owl.payrit.domain.promissorynote.controller;
 
+import com.owl.payrit.domain.promissorynote.dto.request.PromissoryNoteRequest;
 import com.owl.payrit.domain.promissorynote.service.PromissoryNoteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/paper")
 public class PromissoryNoteRestController {
 
     private final PromissoryNoteService promissoryNoteService;
+
+    @PostMapping("/write")
+    public ResponseEntity<String> createNote(@RequestBody PromissoryNoteRequest promissoryNoteRequest,
+                                             Principal principal) {
+
+        promissoryNoteService.createNote(principal, promissoryNoteRequest);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
