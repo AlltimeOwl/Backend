43391764c9668c7f58b9190216ad3cdbb6fdeaab
SeoMoonk <sunghoon1216@naver.com>
2024-03-05 21:21:14 +0900

    feat: creditorPaperResponse 작성

---
+++ src/main/java/com/owl/payrit/domain/promissorypaper/dto/response/CreditorPaperResponse.java
@@ -0,0 +1,25 @@
+package com.owl.payrit.domain.promissorypaper.dto.response;
+
+import com.owl.payrit.domain.promissorypaper.entity.PromissoryPaper;
+
+import java.time.LocalDate;
+
+public record CreditorPaperResponse(
+        long amount,
+        LocalDate repaymentStartDate,
+        LocalDate repaymentEndDate,
+        String debtorName,
+        int dueDate,
+        String paperStatus
+        ) {
+    public CreditorPaperResponse(PromissoryPaper promissoryPaper) {
+        this(
+                promissoryPaper.getAmount(),
+                promissoryPaper.getRepaymentStartDate(),
+                promissoryPaper.getRepaymentEndDate(),
+                promissoryPaper.getDebtor().getName(),      //TODO: 상대가 아직 가입하기 전일 때 이름을 가져오려면?
+                9999,                                       //TODO: DDAY 계산
+                promissoryPaper.getPaperStatus().name()
+        );
+    }
+}
