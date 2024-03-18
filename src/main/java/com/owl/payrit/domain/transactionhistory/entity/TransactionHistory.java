4ce46c748d932f006f6f3c770035ba09d750628f
SeoMoonk <sunghoon1216@naver.com>
2024-03-18 14:07:48 +0900

    feat: transactionHistory entity setting

--- src/main/java/com/owl/payrit/domain/transactionhistory/entity/TransactionHistory.java
+++ src/main/java/com/owl/payrit/domain/transactionhistory/entity/TransactionHistory.java
@@ -1,12 +1,17 @@
 package com.owl.payrit.domain.transactionhistory.entity;
 
+import com.owl.payrit.domain.member.entity.Member;
 import com.owl.payrit.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,4 +20,21 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder(toBuilder = true)
 @ToString(callSuper = true)
 public class TransactionHistory extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member paidMember;
+
+    private LocalDateTime transactionDate;
+
+    private long amount;
+
+    private String contents;            //결제 내용
+
+    private String transactionType;     //결제 유형
+
+    private String approvalNumber;      //승인 번호
+
+    private String orderNumber;         //주문 번호
+
+    private boolean isSuccess;          //결제 성공 여부
 }
