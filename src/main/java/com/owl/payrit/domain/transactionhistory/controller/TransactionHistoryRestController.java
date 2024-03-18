f1d111a31c3fbfa62904833e2c69b8ae1fdefe9c
SeoMoonk <sunghoon1216@naver.com>
2024-03-18 14:17:08 +0900

    feat: transactionHistory save post mapping

--- src/main/java/com/owl/payrit/domain/transactionhistory/controller/TransactionHistoryRestController.java
+++ src/main/java/com/owl/payrit/domain/transactionhistory/controller/TransactionHistoryRestController.java
@@ -1,8 +1,13 @@
 package com.owl.payrit.domain.transactionhistory.controller;
 
+import com.owl.payrit.domain.auth.dto.response.LoginUser;
+import com.owl.payrit.domain.transactionhistory.dto.request.TransactionHistorySaveRequest;
 import com.owl.payrit.domain.transactionhistory.service.TransactionHistoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransactionHistoryRestController {
 
     private final TransactionHistoryService transactionHistoryService;
+
+    @PostMapping("/transaction/save")
+    public ResponseEntity<Void> save(@AuthenticationPrincipal LoginUser loginUser,
+                                     TransactionHistorySaveRequest transactionHistorySaveRequest) {
+
+        log.info(transactionHistorySaveRequest.toString());
+
+        transactionHistoryService.saveHistory(loginUser, transactionHistorySaveRequest);
+
+        return ResponseEntity.noContent().build();
+    }
 
 }
