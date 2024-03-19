3d04779a780f75d9c16b2951e32258ee901c6e83
Shin-sangwon <dnjsml10@gmail.com>
2024-03-19 18:26:31 +0900

    feat : add memo entity

---
+++ src/main/java/com/owl/payrit/domain/memo/entity/Memo.java
@@ -0,0 +1,28 @@
+package com.owl.payrit.domain.memo.entity;
+
+import com.owl.payrit.domain.promissorypaper.entity.PromissoryPaper;
+import com.owl.payrit.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+public class Memo extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "promissory_paper_id")
+    private PromissoryPaper promissoryPaper;
+
+    private Long memberId;
+
+    private String content;
+
+}
