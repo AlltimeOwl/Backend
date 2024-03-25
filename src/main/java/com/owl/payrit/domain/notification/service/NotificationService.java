9908dab83f6ea4b64f8ec61c8c653a546b94de59
Shin-sangwon <dnjsml10@gmail.com>
2024-03-26 00:10:27 +0900

    TODO : 알람 로직

--- src/main/java/com/owl/payrit/domain/notification/service/NotificationService.java
+++ src/main/java/com/owl/payrit/domain/notification/service/NotificationService.java
@@ -14,4 +14,12 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    /*
+    TODO
+    FCM 의존성
+    알람 전송 기능
+    알람 리스트 조회 기능
+    알람 조회 기능 (read = true)
+     */
+
 }
