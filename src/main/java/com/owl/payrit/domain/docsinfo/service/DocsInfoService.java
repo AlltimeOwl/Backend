ece37469e573b6d2c832b9444c739f293184b1f8
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2024-03-25 14:36:34 +0900

    core: azure file storage dependency add

--- src/main/java/com/owl/payrit/domain/docsinfo/service/DocsInfoService.java
+++ src/main/java/com/owl/payrit/domain/docsinfo/service/DocsInfoService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.azure.storage.file.share.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
