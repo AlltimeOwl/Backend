9f9bc683ce87b7c99473474a76c39c476b3c50c4
Shin-sangwon <dnjsml10@gmail.com>
2024-04-07 20:46:12 +0900

    feat : 본인인증 유저 정보들에 대해 암호화

--- src/main/java/com/owl/payrit/domain/member/entity/CertificationInformation.java
+++ src/main/java/com/owl/payrit/domain/member/entity/CertificationInformation.java
@@ -1,6 +1,8 @@
 package com.owl.payrit.domain.member.entity;
 
+import com.owl.payrit.global.encryption.PromissoryPaperStringConverter;
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -19,15 +21,19 @@ public class CertificationInformation {
     private String impUid;
 
     @Column(name = "certification_name")
+    @Convert(converter = PromissoryPaperStringConverter.class)
     private String name;
 
     @Column(name = "certification_gender")
+    @Convert(converter = PromissoryPaperStringConverter.class)
     private String gender;
 
     @Column(name = "certification_birthday")
+    @Convert(converter = PromissoryPaperStringConverter.class)
     private String birthday;
 
     @Column(name = "certification_phone_number")
+    @Convert(converter = PromissoryPaperStringConverter.class)
     private String phone;
 
 }
