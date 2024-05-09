f36699a48f4265790063ae1ffbfa8d9de1444f03
SeoMoonk <sunghoon1216@naver.com>
2024-05-09 12:58:54 +0900

    remove: remove transaction history test controller for dev

--- src/main/java/com/owl/payrit/domain/repaymenthistory/dto/request/RepaymentCancelRequest.java
+++ src/main/java/com/owl/payrit/domain/repaymenthistory/dto/request/RepaymentCancelRequest.java
@@ -1,7 +1,13 @@
 package com.owl.payrit.domain.repaymenthistory.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RepaymentCancelRequest(
+        
+        @Schema(description = "상환이 기록되었던 차용증 id")
         Long paperId,
+
+        @Schema(description = "상환 기록의 id")
         Long historyId
 ) {
 }
